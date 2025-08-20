b2f9a576d355d9b0ee77283c9ce533c9c8764376
MINRYUL <rud0806@gmail.com>
2025-08-20 15:19:54 +0900

    Template > Chekbox Label 추가

---
+++ Sources/ShoplDesignGuide/Template/CheckBoxLabel/Model/CheckBoxLabelModel.swift
@@ -0,0 +1,49 @@
+//
+//  CheckBoxLabelModel.swift
+//  ShoplDesignGuide
+//
+//  Created by Jerry on 8/20/25.
+//
+
+import Foundation
+
+public struct CheckBoxLabelModel: Equatable, Identifiable {
+  public enum LabelColorOption: Equatable {
+    case option1
+    case option2
+  }
+
+  public enum `Type`: Equatable {
+    case normal
+    case empha
+  }
+
+  public let id: String
+  public let labelColorOption: LabelColorOption
+  public let type: `Type`
+  public let title: String
+  public var isSelectedColorPrimary: Bool
+  public var lineLimit: Int
+  public var isSelected: Bool
+  public var isDisabled: Bool
+
+  public init(
+    id: String,
+    labelColorOption: LabelColorOption,
+    type: `Type`,
+    title: String,
+    isSelectedColorPrimary: Bool = true,
+    lineLimit: Int = 1,
+    isSelected: Bool,
+    isDisabled: Bool = false
+  ) {
+    self.id = id
+    self.labelColorOption = labelColorOption
+    self.type = type
+    self.title = title
+    self.isSelectedColorPrimary = isSelectedColorPrimary
+    self.lineLimit = lineLimit
+    self.isSelected = isSelected
+    self.isDisabled = isDisabled
+  }
+}
