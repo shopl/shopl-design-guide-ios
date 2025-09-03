b0d8b839010957f1cdf5e070b4ac22ed07bff741
MINRYUL <rud0806@gmail.com>
2025-09-03 14:46:27 +0900

    Spacing 정의

---
+++ Sources/ShoplDesignGuide/Foundation/SDGSpacing/SDGSpacing.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  ShoplDesignGuide
+//
+//  Created by Jerry on 9/3/25.
+//
+
+import Foundation
+
+public enum SDGSpacing: CGFloat {
+  
+  //MARK: - Common
+  case spacing2 = 2.0
+  case spacing4 = 4.0
+  case spacing8 = 8.0
+  case spacing10 = 10.0
+  case spacing12 = 12.0
+  case spacing16 = 16.0
+  case spacing20 = 20.0
+  case spacing24 = 24.0
+  case spacing40 = 40.0
+  
+  //MARK: - Special
+  case spacing6 = 6.0
+  case spacing28 = 28.0
+  case spacing32 = 32.0
+  case spacing60 = 60.0
+  case spacing100 = 100.0
+  case spacing104 = 104.0
+  
+}
