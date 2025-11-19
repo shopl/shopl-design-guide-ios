fbcc044b50c46af54d6aebb59ebf2a52998022e6
Dino Park <roadcodesoar@gmail.com>
2025-11-19 17:37:58 +0900

    iOS 16 > UIKit의 뷰 컨트롤러 전환(Presentation) 로직과 SwiftUI의 애니메이션(withAnimation) 로직이 충돌하여 발생한 데드락으로 인한 프리징 수정

--- Sources/ShoplDesignGuide/Template/Popup/Util/PopupAnimation.swift
+++ Sources/ShoplDesignGuide/Template/Popup/Util/PopupAnimation.swift
@@ -12,9 +12,13 @@ public enum PopupAnimation {
   var presentDuration: Double {
     switch self {
     case .fadeInOut:
-      0.0
+      if #available(iOS 17, *) {
+        return 0
+      } else {
+        return 0.1 // iOS 16 이하에서만 발생하는 데드락 회피 위한 지연
+      }
     case .slideBottomTop:
-      0.4
+      return 0.4
     }
   }
   var dismissDuration: Double {
