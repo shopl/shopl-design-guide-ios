7095d1978c07517a8e77db9c90e3b36b93eccaed
dino <roadcodesoar@gmail.com>
2026-05-21 15:14:45 +0900

    feat: 디자인 가이드에 맞게 xsmall 추가 및 large 제거

--- Sources/ShoplDesignGuide/Component/IconLabel/SDGIconLabel.swift
+++ Sources/ShoplDesignGuide/Component/IconLabel/SDGIconLabel.swift
@@ -8,38 +8,38 @@
 import SwiftUI
 
 public struct SDGIconLabel: View {
-  
+
   public enum Size: Equatable {
-    case large(isBold: Bool)
     case medium(isBold: Bool)
     case small(isBold: Bool)
-    
+    case xsmall(isBold: Bool)
+
     var typoSize: SDG.Typography {
       switch self {
-      case let .large(isBold): return isBold ? .body1_SB : .body1_R
-      case let .medium(isBold): return isBold ? .body2_SB : .body2_R
-      case let .small(isBold): return isBold ? .body3_SB : .body3_R
+      case let .medium(isBold): return isBold ? .body1_SB : .body1_R
+      case let .small(isBold): return isBold ? .body2_SB : .body2_R
+      case let .xsmall(isBold): return isBold ? .body3_SB : .body3_R
       }
     }
-    
+
     public static func == (lhs: SDGIconLabel.Size, rhs: SDGIconLabel.Size) -> Bool {
       switch (lhs, rhs) {
-      case (.large(let lhsIsBold), .large(let rhsIsBold)):
-        return lhsIsBold == rhsIsBold
       case (.medium(let lhsIsBold), .medium(let rhsIsBold)):
         return lhsIsBold == rhsIsBold
       case (.small(let lhsIsBold), .small(let rhsIsBold)):
+        return lhsIsBold == rhsIsBold
+      case (.xsmall(let lhsIsBold), .xsmall(let rhsIsBold)):
         return lhsIsBold == rhsIsBold
       default: return false
       }
     }
   }
-  
+
   public enum Spacing: CGFloat, Equatable {
     case two = 2
     case four = 4
   }
-  
+
   private let size: Size
   private let spacing: Spacing
   private let leftImage: Image?
@@ -49,7 +49,7 @@ public struct SDGIconLabel: View {
   private let titleColor: SDG.Color
   private let lineLimit: Int?
   private let fullSize: Bool
-  
+
   public init(
     size: Size,
     spacing: Spacing = .two,
@@ -71,7 +71,7 @@ public struct SDGIconLabel: View {
     self.lineLimit = lineLimit
     self.fullSize = fullSize
   }
-  
+
   public var body: some View {
     HStack(alignment: .top, spacing: self.spacing.rawValue) {
       if let image = leftImage {
@@ -81,24 +81,24 @@ public struct SDGIconLabel: View {
           .foregroundStyle(self.imageForegroundColor)
           .padding(.top, 1)
       }
-      
-      Text(title)
-        .typo(size.typoSize, titleColor)
-        .applyIf(self.fullSize) {
-          $0.frame(maxWidth: .infinity, alignment: .leading)
-        }
-        .multilineTextAlignment(.leading)
-        .lineLimit(self.lineLimit)
-        .fixedSize(horizontal: false, vertical: true)
-      
-      if let image = rightImage {
-        image
-          .resizable()
-          .frame(width: 14, height: 14)
-          .foregroundStyle(self.imageForegroundColor)
-          .padding(.top, 1)
+
+    Text(title)
+      .typo(size.typoSize, titleColor)
+      .applyIf(self.fullSize) {
+        $0.frame(maxWidth: .infinity, alignment: .leading)
       }
-    }
+      .multilineTextAlignment(.leading)
+      .lineLimit(self.lineLimit)
+      .fixedSize(horizontal: false, vertical: true)
+
+      if let image = rightImage {
+    image
+      .resizable()
+      .frame(width: 14, height: 14)
+      .foregroundStyle(self.imageForegroundColor)
+      .padding(.top, 1)
+  }
+}
   }
 }
 
@@ -113,7 +113,7 @@ public struct SDGIconLabel: View {
       lineLimit: 1,
       fullSize: true
     )
-    
+
     SDGIconLabel(
       size: .medium(isBold: false),
       leftImage: Image(sdg: .icCommonWarning),
@@ -122,7 +122,7 @@ public struct SDGIconLabel: View {
       titleColor: .red300,
       fullSize: true
     )
-    
+
     SDGIconLabel(
       size: .medium(isBold: false),
       rightImage: Image(sdg: .icView),
@@ -131,10 +131,10 @@ public struct SDGIconLabel: View {
       titleColor: .neutral400,
       lineLimit: 1
     )
-    
+
     
     SDGIconLabel(
-      size: .large(isBold: false),
+      size: .xsmall(isBold: false),
       spacing: .four,
       leftImage: Image(sdg: .icCommonWarning),
       title: "텍스트가 길어질 경우 전체 노출이 필요한 경우는 줄바꿈이 될텐데 그랬을 때는 아이콘의 위치는 상단에 고정시킵니다.",
