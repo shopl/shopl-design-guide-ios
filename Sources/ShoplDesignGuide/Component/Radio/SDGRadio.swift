70a46d256ed5e6f93570ede9891cb0fd3375d41b
jerry <rud0806@gmail.com>
2026-06-26 12:38:12 +0900

    feat: Radio 디자인 시스템 반영

--- Sources/ShoplDesignGuide/Component/Radio/SDGRadio.swift
+++ Sources/ShoplDesignGuide/Component/Radio/SDGRadio.swift
@@ -8,41 +8,135 @@
 import SwiftUI
 
 public struct SDGRadio: View {
+  public static let version = "2.0.0"
   
-  private let _isSelected: Bool
-  private let _size: CGFloat
-  private let _isDisabled: Bool
+  public enum Spec: Equatable {
+    case large
+    case medim
   
-  public init(isSelected: Bool,
-              size: CGFloat = 16,
-              isDisabled: Bool = false) {
-    _isSelected = isSelected
-    _size = size
-    _isDisabled = isDisabled
+    fileprivate var iconSize: CGFloat {
+      switch self {
+      case .large: return 16
+      case .medim: return 14
+      }
+    }
+  }
+  
+  public struct Model: Equatable {
+    public var status: SDGRadioStatus
+    public let spec: Spec
+    public let isPrimaryColor: Bool
+    
+    public init(status: SDGRadioStatus, spec: Spec, isPrimaryColor: Bool = true) {
+      self.status = status
+      self.spec = spec
+      self.isPrimaryColor = isPrimaryColor
+    }
+    
+    var backgroundColor: Color {
+      return isPrimaryColor ? .primary300 : .neutral700
+    }
+  }
+  
+  private var model: Model
+  private var selected: (() -> ())
+  
+  public init(
+    model: Model,
+    selected: @escaping (() -> ())
+  ) {
+    self.model = model
+    self.selected = selected
   }
   
   public var body: some View {
     
     ZStack(alignment: .center) {
       Circle()
-        .frame(width: _size, height: _size)
-        .foregroundColor(_isSelected ? .primary300 : .neutral200)
-        .applyIf(_isDisabled) {
+        .frame(width: model.spec.iconSize, height: model.spec.iconSize)
+        .foregroundColor(
+          model.status == .selected ?
+          model.backgroundColor :
+            .neutral200
+        )
+        .applyIf(model.status == .disabled) {
           $0.foregroundStyle(.neutral200)
         }
       
       Circle()
-        .frame(width: _size / 2, height: _size / 2)
+        .frame(width: model.spec.iconSize / 2, height: model.spec.iconSize / 2)
         .foregroundColor(.neutral0)
     }
   }
 }
 
-struct SDGRadio_Previews: PreviewProvider {
-  static var previews: some View {
-    VStack(spacing: 10) {
-      SDGRadio(isSelected: true)
-      SDGRadio(isSelected: false)
+#Preview {
+  VStack(alignment: .leading, spacing: 16) {
+    VStack(alignment: .leading, spacing: 8) {
+      Text("Large / Primary")
+        .typo(.body1_SB, .neutral500)
+      
+      HStack(spacing: 16) {
+        SDGRadio(
+          model: .init(status: .default, spec: .large),
+          selected: { }
+        )
+        
+        SDGRadio(
+          model: .init(status: .selected, spec: .large),
+          selected: { }
+        )
+        
+        SDGRadio(
+          model: .init(status: .disabled, spec: .large),
+          selected: { }
+        )
+      }
+    }
+    
+    VStack(alignment: .leading, spacing: 8) {
+      Text("Medium / Primary")
+        .typo(.body1_SB, .neutral500)
+      
+      HStack(spacing: 16) {
+        SDGRadio(
+          model: .init(status: .default, spec: .medim),
+          selected: { }
+        )
+        
+        SDGRadio(
+          model: .init(status: .selected, spec: .medim),
+          selected: { }
+        )
+        
+        SDGRadio(
+          model: .init(status: .disabled, spec: .medim),
+          selected: { }
+        )
+      }
+    }
+    
+    VStack(alignment: .leading, spacing: 8) {
+      Text("Large / Neutral")
+        .typo(.body1_SB, .neutral500)
+      
+      HStack(spacing: 16) {
+        SDGRadio(
+          model: .init(status: .default, spec: .large, isPrimaryColor: false),
+          selected: { }
+        )
+        
+        SDGRadio(
+          model: .init(status: .selected, spec: .large, isPrimaryColor: false),
+          selected: { }
+        )
+        
+        SDGRadio(
+          model: .init(status: .disabled, spec: .large, isPrimaryColor: false),
+          selected: { }
+        )
+      }
     }
   }
+  .padding(20)
 }
--- Sources/ShoplDesignGuide/Template/RadioLabel/SDGRadioLabel.swift
+++ Sources/ShoplDesignGuide/Template/RadioLabel/SDGRadioLabel.swift
@@ -11,20 +11,26 @@ public struct SDGRadioLabel: View {
   
   public var model: RadioLabelModel
   
+  private var radioStatus: SDGRadioStatus {
+    if model.isDisabled { return .disabled }
+    if model.isSelected { return .selected }
+    return .default
+  }
+  
+  private var radioModel: SDGRadio.Model {
+    return SDGRadio.Model(
+      status: radioStatus,
+      spec: .large,
+      isPrimaryColor: !model.isSelectedColorNeturel
+    )
+  }
+  
   private var textColor: SDG.Color {
     if model.isDisabled { return .neutral200 }
     if model.isLabelColorPrimary { return .primary300 }
     return .neutral700
   }
-  
-  private var selectedColor: Color {
-    if model.isSelectedColorNeturel {
-      return .neutral700
-    }
-    
-    return .primary300
-  }
-  
+
   private var onTap: (String) -> Void
   
   public init(
@@ -42,14 +48,10 @@ public struct SDGRadioLabel: View {
       
     } label : {
       HStack(alignment: .top, spacing: 8) {
-        ZStack {
-          Color.neutral0
-            .frame(width: 8, height: 8)
-            .cornerRadius(4)
-            .padding(4)
-        }
-        .background(model.isSelected ? selectedColor : .neutral200)
-        .cornerRadius(8)
+        SDGRadio(
+          model: radioModel,
+          selected: { }
+        )
         .padding(.top, 1)
         
         Text(model.title)
@@ -60,6 +62,8 @@ public struct SDGRadioLabel: View {
           .fixedSize(horizontal: false, vertical: true)
       }
     }
+    .buttonStyle(NoTapAnimationButtonStyle())
+    .disabled(model.isDisabled)
   }
 }
 
