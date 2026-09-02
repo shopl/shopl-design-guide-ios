bd5bf1423fe0e8e99b3a4097fc19e49c734662fc
dino <roadcodesoar@gmail.com>
2026-09-03 08:27:31 +0900

    refactor: 멀티 타임 피커 시간 변환 타입 파일 분리

--- Sources/ShoplDesignGuide/Template/MultiTimePicker/SDGMultiTimePicker.swift
+++ Sources/ShoplDesignGuide/Template/MultiTimePicker/SDGMultiTimePicker.swift
@@ -265,34 +265,3 @@ public struct SDGMultiTimePicker: View {
     )
   }
 }
-
-enum SDGTimeValueDateBridge {
-  static let timeZone = TimeZone.gmt
-
-  static let calendar: Calendar = {
-    var calendar = Calendar(identifier: .gregorian)
-    calendar.timeZone = timeZone
-    return calendar
-  }()
-
-  private static let referenceDate = Date(timeIntervalSinceReferenceDate: 0)
-
-  static func date(from time: SDGTimeValue) -> Date {
-    return referenceDate.addingTimeInterval(
-      TimeInterval(time.hour * 60 * 60 + time.minute * 60)
-    )
-  }
-
-  static func time(from date: Date) -> SDGTimeValue {
-    let components = calendar.dateComponents([.hour, .minute], from: date)
-
-    guard let hour = components.hour,
-      let minute = components.minute,
-      let time = SDGTimeValue(hour: hour, minute: minute)
-    else {
-      return .midnight
-    }
-
-    return time
-  }
-}
